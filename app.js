2b5cc57981c5b7b5918bcfe28378f25478c187ab
ayoub3bidi <mrayoubabidi@gmail.com>
2021-09-24 22:29:54 +0100

    ⚡️ Connecting to MongoDB

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = '';
+const dbURI = 'mongodb+srv://Bouba:j3OAA7dwtVtgNvuk@cluster0.gtg90.mongodb.net/node-auth';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));

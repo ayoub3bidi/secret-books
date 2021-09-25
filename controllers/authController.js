859a1c0703533af99c656a3ecf89ed3875c96abd
ayoub3bidi <mrayoubabidi@gmail.com>
2021-09-25 14:21:21 +0100

    ✅ User model

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,32 @@
+   
+const User = require("../models/User");
+
 // controller actions
 module.exports.signup_get = (req, res) => {
-    res.render('signup');
+  res.render('signup');
+}
+
+module.exports.login_get = (req, res) => {
+  res.render('login');
+}
+
+module.exports.signup_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
   }
-  
-  module.exports.login_get = (req, res) => {
-    res.render('login');
+  catch(err) {
+    console.log(err);
+    res.status(400).send('error, user not created');
   }
-  
-  module.exports.signup_post = async (req, res) => {
-    res.send('new signup');
-  }
-  
-  module.exports.login_post = async (req, res) => {
-    res.send('user login');
-  }
+ 
+}
+
+module.exports.login_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  console.log(email, password);
+  res.send('user login');
+}

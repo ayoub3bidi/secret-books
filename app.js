da50fce5b9f07935d980417b6a28cc9b278acd6d
ayoub3bidi <mrayoubabidi@gmail.com>
2021-09-26 16:39:43 +0100

    ✅ Protecting routes

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth } = require('./middleware/authMiddleware');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -19,29 +22,5 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 
 // routes
 app.get('/', (req, res) => res.render('home'));
-app.get('/books', (req, res) => res.render('books'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
-
-// cookies
-const cookieParser = require('cookie-parser');
-app.use(cookieParser());
-
-app.get('/set-cookies', (req, res) => {
-
-  // res.setHeader('Set-Cookie', 'newUser=true');
-  
-  res.cookie('newUser', false);
-  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
-
-  res.send('you got the cookies!');
-
-});
-
-app.get('/read-cookies', (req, res) => {
-
-  const cookies = req.cookies;
-  console.log(cookies.newUser);
-
-  res.json(cookies);
-
-});
